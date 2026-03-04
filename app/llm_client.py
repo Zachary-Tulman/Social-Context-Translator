d2a0bdfc7cedbc0005eaa1a678169e1a84c0c1ba
unknown <zrtulman@gmail.com>
2026-03-03 22:23:52 -0800

    Added RAG code retrieving from vectorstore/ and adding to system prompt

--- app/llm_client.py
+++ app/llm_client.py
@@ -1,16 +1,31 @@
 from langchain_anthropic import ChatAnthropic
 from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 llm = ChatAnthropic(model_name="claude-sonnet-4-6", timeout=60, stop=[])
+embed = HuggingFaceEmbeddings()
+db = Chroma(persist_directory="vectorstore/", embedding_function=embed).as_retriever()
+
+def __get_system_prompt(message: str) -> str:
+    system_prompt = str(os.getenv("SYSTEM_PROMPT"))
+    chunks = db.invoke(message)
+    for chunk in chunks:
+        system_prompt += "\n"
+        system_prompt += chunk.page_content
+    
+    return system_prompt
 
 def chat(message: str, conversation_history: list) -> str:
     conversation_history.append(HumanMessage(content=message))
 
-    messages = [SystemMessage(content=os.getenv("SYSTEM_PROMPT"))] + conversation_history
+    system_prompt = __get_system_prompt(message)
+
+    messages = [SystemMessage(system_prompt)] + conversation_history
 
     response = llm.invoke(messages)
 
